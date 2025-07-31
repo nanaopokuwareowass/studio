bf57c4061a171d3f867e63e1db8d97c51bc662cf
Opoku-Ware Abdallah <nanaopokuwareowass@gmail.com>
2025-07-31 12:27:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -86,7 +86,7 @@ const MainSidebarContent = () => (
                             <span>Analytics</span>
                         </SidebarMenuButton>
                     </Link>
-                </IframeMenuItem>
+                </SidebarMenuItem>
                  <SidebarMenuItem>
                      <Link href="/dashboard/admin/users" passHref>
                         <SidebarMenuButton>
