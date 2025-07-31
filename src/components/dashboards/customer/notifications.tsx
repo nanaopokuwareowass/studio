7c870c2e68b2749ecbcb16214a71ca74c39531e7
Opoku-Ware Abdallah <nanaopokuwareowass@gmail.com>
2025-07-31 12:42:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/dashboards/customer/notifications.tsx
+++ src/components/dashboards/customer/notifications.tsx
@@ -1,7 +1,7 @@
 
 "use client"
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
